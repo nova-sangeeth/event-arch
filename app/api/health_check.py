33470a2f5ec281ea47bc26eb6e408084b093d1e3
nova-sangeeth <novasangeeth@outlook.com>
2023-08-16 04:20:38 +0530

    Add comments to the endpoint

--- app/api/health_check.py
+++ app/api/health_check.py
@@ -5,4 +5,7 @@ router = APIRouter()
 
 @router.get("/health-check/", tags=["Health Check"])
 async def read_users():
-    return {"data": "OK"}
+    # TODO: Make changes to improve the endpoint to check the db connection.
+    # TODO: Make changes to improve the endpoint to check the Redis Cache.
+
+    return {"reponse": "OK"}
