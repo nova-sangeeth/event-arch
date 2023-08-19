5e9ff1c4b9ed7debd00dcd29d647436a86f2da4a
nova-sangeeth <novasangeeth@outlook.com>
2023-08-19 23:56:56 +0530

    Changes to the doc strings

---
+++ app/api/auth/__init__.py
@@ -0,0 +1,9 @@
+"""
+Backend : api : authentication
+================================
+This package contains the api endpoints for the backend applicationD
+"""
+
+__author__ = "novasangeeth@outlook.com"
+
+# Constants for the endpoints
---
+++ app/orm/__init__.py
@@ -0,0 +1,8 @@
+"""
+Backend : orm
+===============
+This package contains the SQLAlchemy ORM model
+"""
+__author__ = 'novasangeeth@outlook.com'
+
+# Add the necessary constants here
