19fb588e22410ce5dd023d11490352b5cb0bab19
nova-sangeeth <novasangeeth@outlook.com>
2023-08-22 02:36:50 +0530

    Doc strings for the authentication model

---
+++ app/api/auth/forgot_pasword.py
@@ -0,0 +1,9 @@
+"""
+Backend : api : authentication : Forgot Password
+==================================================
+This module contains the endpoints for forgot password
+"""
+
+__author__ = "novasangeeth@outlook.com"
+
+# The forgot password endpoint
