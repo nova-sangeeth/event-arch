73f4be8336dea16009fbb32012ec155001bd9414
nova-sangeeth <novasangeeth@outlook.com>
2023-08-23 00:49:19 +0530

    Changes to start work with the Login endpoints

---
+++ app/api/auth/login.py
@@ -0,0 +1,9 @@
+"""
+Backend : api : authentication : login
+=======================================
+This module contains the endpoints for login
+"""
+
+__author__ = "novasangeeth@outlook.com"
+
+# The login endpoint
