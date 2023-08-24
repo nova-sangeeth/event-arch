a38bc9074654375c5c7d7c8f0f8ff303e9d497fa
nova-sangeeth <novasangeeth@outlook.com>
2023-08-24 23:54:33 +0530

    Add doc strings

---
+++ app/api/auth/logout.py
@@ -0,0 +1,9 @@
+"""
+Backend : api : authentication : Logout
+=======================================
+This module contains the endpoints for logout
+"""
+
+__author__ = "novasangeeth@outlook.com"
+
+# The logout endpoint
