384f5df2c2d152a5c3ba8670b4d3d461e4782bf0
nova-sangeeth <novasangeeth@outlook.com>
2023-08-26 00:42:14 +0530

    Changes to schema and orm

---
+++ app/orm/auth/roles.py
@@ -0,0 +1,8 @@
+"""
+Backend : authentication : Roles
+==================================
+This module contains the SQLAlchemy models for user roles
+"""
+__author__ = 'novasangeeth@outlook.com'
+
+# The user roles model
---
+++ app/schema/auth/permissions.py
@@ -0,0 +1,7 @@
+"""
+Backend : schema : permissions
+===============================
+This module contains the schemas for permissions
+"""
+
+__author__ = "novasangeeth@outlook.com"
