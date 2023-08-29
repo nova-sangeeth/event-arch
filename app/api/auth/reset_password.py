d3f31b98df05a49dbaef5212dbee08f355531330
nova-sangeeth <novasangeeth@outlook.com>
2023-08-29 23:42:08 +0530

    Changes to add permissions and reset_password

---
+++ app/api/auth/reset_password.py
@@ -0,0 +1,9 @@
+"""
+Backend : api : authentication : Reset Password
+=======================================
+This module contains the endpoints for reset password
+"""
+
+__author__ = "novasangeeth@outlook.com"
+
+# The reset password endpoint
---
+++ app/orm/auth/permissions.py
@@ -0,0 +1,8 @@
+"""
+Backend : orm : authentication : permissions
+====================================
+This module contains the SQLAlchemy models for user models
+"""
+__author__ = 'novasangeeth@outlook.com'
+
+# The permissions model
