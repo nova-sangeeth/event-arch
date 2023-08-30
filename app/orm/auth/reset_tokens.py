164eadf152aca6a163c358e0c870a8d78fa363bc
nova-sangeeth <novasangeeth@outlook.com>
2023-08-31 00:48:08 +0530

    Changes to reset tokens

---
+++ app/orm/auth/reset_tokens.py
@@ -0,0 +1,8 @@
+"""
+Backend : auth : Reset Tokens
+===============================
+This module contains the SQLAlchemy models for password reset tokens
+"""
+__author__ = 'novasangeeth@outlook.com'
+
+# The password reset token model
