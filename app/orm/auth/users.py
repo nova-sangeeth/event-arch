fc54d198769bfb56af3d251eee4d1239a386985c
nova-sangeeth <novasangeeth@outlook.com>
2023-08-31 22:43:56 +0530

    doc strings for the reset tokens

---
+++ app/orm/auth/users.py
@@ -0,0 +1,9 @@
+"""
+Backend : orm : authentication : Users
+========================================
+This module contains the SQLAlchemy models for user authentication
+"""
+__author__ = 'novasangeeth@outlook.com'
+
+# The user model
+# The user roles model
