00279705bbbe36f506a7e18c3228940325fd88d9
nova-sangeeth <novasangeeth@outlook.com>
2023-09-18 01:44:26 +0530

    Start writing the User ORM class

--- app/orm/auth/users.py
+++ app/orm/auth/users.py
@@ -3,7 +3,21 @@ Backend : orm : authentication : Users
 ========================================
 This module contains the SQLAlchemy models for user authentication
 """
-__author__ = 'novasangeeth@outlook.com'
+__author__ = "novasangeeth@outlook.com"
 
-# The user model
-# The user roles model
+from app.db.db import Base
+from sqlalchemy import Column
+from sqlalchemy import Integer, String
+
+
+class User(Base):
+    """
+    The user ORM model class
+    """
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, )
+    username = Column(String(length=255), nullable=False, unique=True, )
+    email = Column(String(length=255), nullable=True, unique=True, )
+    first_name = Column(String(length=255), nullable=False, unique=False, )
+    middle_name = Column(String(length=255), nullable=True, unique=False, )
+    last_name = Column(String(length=255), nullable=True, unique=False, )
