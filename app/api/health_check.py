292f536b62c81505bb753bc62dc4c2a017fccb9f
nova-sangeeth <novasangeeth@outlook.com>
2023-09-19 23:54:17 +0530

    Changes to the endpoint

--- app/api/health_check.py
+++ app/api/health_check.py
@@ -8,13 +8,16 @@ from fastapi import APIRouter
 
 __author__ = "novasangeeth@outlook.com"
 
-
+# Initialize the health check router
 router = APIRouter()
 
 
 @router.get("/health-check/", tags=["Health Check"])
-async def read_users():
+def health_check() -> str:
+    """
+    An endpoint to check the application's health
+    """
     # TODO: Make changes to improve the endpoint to check the db connection.
     # TODO: Make changes to improve the endpoint to check the Redis Cache.
 
-    return {"reponse": "OK"}
+    return {"msg": "OK"}
