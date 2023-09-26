e11fbbfd3e95f88107c4f4a511faed539dc5a798
nova-sangeeth <novasangeeth@outlook.com>
2023-09-27 00:12:28 +0530

    initiate the changes for microservice within the Declarative base.

--- app/db/db.py
+++ app/db/db.py
@@ -3,5 +3,11 @@ Backend : db
 ==============
 This package contains the configuration for db 
 """
-__author__ = 'novasangeeth@outlook.com'
+__author__ = "novasangeeth@outlook.com"
 
+
+from sqlalchemy.ext.declarative import declarative_base
+
+# Initialize the declarative Base
+
+Base = declarative_base()
