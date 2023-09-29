39b116535d9dda1bb056c9f8db3475f6d0334d6a
nova-sangeeth <novasangeeth@outlook.com>
2023-09-29 23:17:14 +0530

    add injest

--- app/db/injest.py
+++ app/db/injest.py
@@ -3,5 +3,6 @@ Backend : db : injest
 =======================
 This package contains the methods for injestion
 """
-__author__ = 'novasangeeth@outlook.com'
+__author__ = "novasangeeth@outlook.com"
 
+# Functions used to add data to the database on app intialization.
