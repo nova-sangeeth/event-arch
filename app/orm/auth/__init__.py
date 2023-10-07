68fef33085429926fcad42ac992778079a6b0c68
nova-sangeeth <novasangeeth@outlook.com>
2023-10-07 23:56:24 +0530

    Add auth

--- app/orm/auth/__init__.py
+++ app/orm/auth/__init__.py
@@ -3,5 +3,4 @@ Backend : orm : authentication
 ================================
 This package contains the  SQLAlchemy ORM models for Authentication
 """
-__author__ = 'novasangeeth@outlook.com'
-
+__author__ = "novasangeeth@outlook.com"
