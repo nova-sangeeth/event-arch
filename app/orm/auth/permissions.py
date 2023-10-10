d251184e6c1b660e74a0cd6c5c60e44858abcfcb
nova-sangeeth <novasangeeth@outlook.com>
2023-10-10 11:36:58 +0530

    ad permissions.

--- app/orm/auth/permissions.py
+++ app/orm/auth/permissions.py
@@ -3,6 +3,18 @@ Backend : orm : authentication : permissions
 ====================================
 This module contains the SQLAlchemy models for user models
 """
-__author__ = 'novasangeeth@outlook.com'
+__author__ = "novasangeeth@outlook.com"
 
-# The permissions model
+# The user roles model
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from app.db.db import Base
+
+
+class Permissions(Base):
+    """
+    The permission ORM model class
+    """
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    permission = Column(String(length=128), nullable=True, unique=True)
