cc10233e411c6b58350049fb0903b0f8c9b4b246
nova-sangeeth <novasangeeth@outlook.com>
2023-10-16 16:51:29 +0530

    add roles.

--- app/orm/auth/roles.py
+++ app/orm/auth/roles.py
@@ -3,6 +3,18 @@ Backend : authentication : Roles
 ==================================
 This module contains the SQLAlchemy models for user roles
 """
-__author__ = 'novasangeeth@outlook.com'
+__author__ = "novasangeeth@outlook.com"
 
 # The user roles model
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from app.db.db import Base
+
+
+class Roles(Base):
+    """
+    The roles ORM model class
+    """
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    role_name = Column(String(length=128), nullable=False, unique=True)
