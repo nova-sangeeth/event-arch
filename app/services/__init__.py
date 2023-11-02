666683c0fc0cc2818f575e4de9ae849f62673521
nova-sangeeth <novasangeeth@outlook.com>
2023-11-02 21:24:59 +0530

    Add files for services auth.

---
+++ app/services/__init__.py
@@ -0,0 +1,7 @@
+"""
+Backend : Services : Authentication
+====================================
+This package contains the services for authentication.
+"""
+
+__author__ = "novasangeeth@outlook.com"
