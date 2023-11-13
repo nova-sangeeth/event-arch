92b3b217b5fc2b17923b1873df0a3243b21f49b2
nova-sangeeth <novasangeeth@outlook.com>
2023-11-14 00:15:16 +0530

    endpoint for forgot-password

--- app/api/auth/forgot_pasword.py
+++ app/api/auth/forgot_pasword.py
@@ -3,7 +3,20 @@ Backend : api : authentication : Forgot Password
 ==================================================
 This module contains the endpoints for forgot password
 """
+from fastapi import APIRouter
 
 __author__ = "novasangeeth@outlook.com"
 
+router = APIRouter(prefix="/auth")
 # The forgot password endpoint
+
+
+@router.post(
+    path="/forgot-password",
+    tag=["Auth"],
+)
+def api_forgot_password():
+    """
+    Endpoint to forgot password.
+    """
+    return "OK"
