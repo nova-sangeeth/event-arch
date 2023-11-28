f8e39c68356a83f0bfc1a9bf34c0bfc116ac18b2
nova-sangeeth <novasangeeth@outlook.com>
2023-11-29 00:03:17 +0530

    Function for init db

--- app/db/injest.py
+++ app/db/injest.py
@@ -6,3 +6,10 @@ This package contains the methods for injestion
 __author__ = "novasangeeth@outlook.com"
 
 # Functions used to add data to the database on app intialization.
+
+
+def init_db():
+    """
+    Initialize the database.
+    """
+    pass
