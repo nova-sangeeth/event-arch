b34dd9ac0b3192dc3f34ec2488d93e312244c5f1
nova-sangeeth <novasangeeth@outlook.com>
2023-11-28 23:55:13 +0530

    Changes to the auth endpoints.

--- app/api/auth/login.py
+++ app/api/auth/login.py
@@ -4,6 +4,19 @@ Backend : api : authentication : login
 This module contains the endpoints for login
 """
 
+from fastapi import APIRouter
+
+
 __author__ = "novasangeeth@outlook.com"
 
 # The login endpoint
+
+router = APIRouter(prefix="/auth")
+
+
+@router.post(path="/login", tags=["Auth"])
+def api_login():
+    """
+    Endpoint to login to the system.
+    """
+    return "OK"
