49d33ed29f3358f1d46e5f61c7ee06024bc90ccf
nova-sangeeth <novasangeeth@outlook.com>
2023-11-30 00:56:26 +0530

    Changes to the logout api

--- app/api/auth/logout.py
+++ app/api/auth/logout.py
@@ -3,7 +3,21 @@ Backend : api : authentication : Logout
 =======================================
 This module contains the endpoints for logout
 """
+from fastapi import APIRouter
 
 __author__ = "novasangeeth@outlook.com"
 
+
+router = APIRouter(prefix="/auth")
 # The logout endpoint
+
+
+@router.post(
+    path="/logout",
+    tags=["Auth"],
+)
+def api_logout():
+    """
+    Endpoint to logout to the system.
+    """
+    return "OK"
