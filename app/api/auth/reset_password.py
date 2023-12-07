24a7510a2e3f7ecd2b0a7e61d56d95092517048a
nova-sangeeth <novasangeeth@outlook.com>
2023-12-07 21:51:44 +0530

    API changes for reset-password,

--- app/api/auth/reset_password.py
+++ app/api/auth/reset_password.py
@@ -3,7 +3,20 @@ Backend : api : authentication : Reset Password
 =======================================
 This module contains the endpoints for reset password
 """
+from fastapi import APIRouter
 
 __author__ = "novasangeeth@outlook.com"
 
+router = APIRouter(prefix="/auth")
 # The reset password endpoint
+
+
+@router.post(
+    path="/reset-password",
+    tags=["Auth"],
+)
+def api_reset_password():
+    """
+    Endpoint to reset password.
+    """
+    return "OK"
