7ed6f588941eb58fed83e324a849822e27aa8492
nova-sangeeth <novasangeeth@outlook.com>
2023-12-11 00:22:32 +0530

    Changes to add new constants for the api prefixes.

--- app/constants/api.py
+++ app/constants/api.py
@@ -1,3 +1,21 @@
-"""Constants for API prefixes."""
+"""
+Backend : Constants : Api
+==========================
+This module contains the constants for API endpoints and routers.
+"""
+from enum import Enum
+
+
+__author__ = "novasangeeth@outlook.com"
 
 API_PREFIX = "/api"
+
+
+class ApiTagEnum(Enum, str):
+    """
+    The api endpoint tags
+    """
+
+    #:
+    AUTHENTICATION = "AUTH"
+    HEALTH_CHECK = "HEALTH-CHECK"
